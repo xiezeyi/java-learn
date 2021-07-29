f8365b0c0991e3f73127f38135d90343c287b0f6
“xiezeyi” <xiezy@miligc.com>
2021-07-29 19:47:30 +0800

    docs: [redis]diners服务配置及配置网关服务规则，用来dinners服务通过网关通信

---
+++ redis/food-social-contact-parent/ms-diners/src/main/java/com/imooc/diners/DinersApplication.java
@@ -0,0 +1,17 @@
+package com.imooc.diners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: redis
+ * @description:
+ * @author: 谢泽毅
+ * @create: 2021-07-29 19:38
+ **/
+@SpringBootApplication
+public class DinersApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DinersApplication.class, args);
+    }
+}

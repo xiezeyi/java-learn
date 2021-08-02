a508d184ad6dfa793eb1fbe64cc6624d030dde50
“xiezeyi” <xiezy@miligc.com>
2021-08-02 14:28:53 +0800

    feat: [redis]登出业务逻辑，清空redis用户信息，注意健壮性问题

--- redis/food-social-contact-parent/ms-oauth2-server/src/main/java/com/imooc/oauth2/server/controller/UserController.java
+++ redis/food-social-contact-parent/ms-oauth2-server/src/main/java/com/imooc/oauth2/server/controller/UserController.java
@@ -5,8 +5,13 @@ import com.imooc.comons.model.domain.domain.SignInIdentity;
 import com.imooc.comons.model.domain.vo.SignInDinerInfo;
 import com.imooc.comons.utils.ResultInfoUtil;
 import com.mysql.cj.PreparedQuery;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2RefreshToken;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +29,11 @@ public class UserController {
 
     @Resource
     private HttpServletRequest request;
+
+    @Resource
+    private RedisTokenStore redisTokenStore;
+
+
     /**
      * 获取当前用户信息
      * @param authentication
@@ -39,5 +49,37 @@ public class UserController {
         return ResultInfoUtil.buildSuccess(request.getServletPath(), dinerInfo);
     }
 
-//    http://localhost:8082/user/me?access_token=84b848b8-5aeb-4270-86cf-e390267aa4c6
+//   认证方式一 通过access_token http://localhost:8082/user/me?access_token=84b848b8-5aeb-4270-86cf-e390267aa4c6
+//   方式二 通过authorization
+    /**
+     * 安全退出
+     *
+     * @param access_token
+     * @param authorization
+     * @return
+     */
+    @GetMapping("user/logout")
+    public ResultInfo Logout(String access_token, String authorization) {
+        // 判断access_token 是否为空，为空将authorization 赋值给 access_token
+        if (StringUtils.isBlank(access_token)) {
+            access_token = authorization;
+        }
+        // 判断 authorization 是否为空
+        if (StringUtils.isBlank(access_token)) {
+            return ResultInfoUtil.buildSuccess(request.getServletPath());
+        }
+        // 判断 bearer token 是否为空
+        if (access_token.toLowerCase().contains("bearer".toLowerCase())) {
+            access_token = access_token.toLowerCase().replace("bearer","");
+
+        }
+        // 清除 redis token 信息
+        OAuth2AccessToken oAuth2AccessToken = redisTokenStore.readAccessToken(access_token);
+        if (oAuth2AccessToken != null) {
+            redisTokenStore.removeAccessToken(oAuth2AccessToken);
+            OAuth2RefreshToken refreshToken = oAuth2AccessToken.getRefreshToken();
+            redisTokenStore.removeRefreshToken(refreshToken);
+        }
+        return ResultInfoUtil.buildSuccess(request.getServletPath(), "退出成功");
+    }
 }

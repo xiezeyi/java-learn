64f1f2f83dea274e506fd26f00e5281768f49cc5
“xiezeyi” <xiezy@miligc.com>
2021-08-03 19:54:27 +0800

    feat: [redis]应用抢购代金券:初始化项目和配置实体类

---
+++ redis/food-social-contact-parent/commons/src/main/java/com/imooc/comons/model/domain/pojo/SeckillVouchers.java
@@ -0,0 +1,38 @@
+package com.imooc.comons.model.domain.pojo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import com.imooc.comons.model.domain.base.BaseModel;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+/**
+ * @program: redis
+ * @description: 食客实体类
+ * @author: 谢泽毅
+ * @create: 2021-08-03 19:52
+ **/
+@Setter
+@Getter
+@ApiModel(description = "抢购代金券信息")
+public class SeckillVouchers extends BaseModel {
+
+    @ApiModelProperty("代金券外键")
+    private Integer fkVoucherId;
+    @ApiModelProperty("数量")
+    private int amount;
+    @ApiModelProperty("抢购开始时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    private Date startTime;
+    @ApiModelProperty("抢购结束时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    private Date endTime;
+
+}

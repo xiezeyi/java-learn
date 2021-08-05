39d579544b3d5b25f4ef2d04341f77a0f967b9e4
“xiezeyi” <xiezy@miligc.com>
2021-08-05 13:55:40 +0800

    feat: [redis-lock-demo]添加一个redis lock的demo项目，用于理解redis lock

---
+++ redis/redis-lock-demo/src/main/java/com/imooc/demo02/RedisLockTest2.java
@@ -0,0 +1,30 @@
+package com.imooc.demo02;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.UUID;
+
+public class RedisLockTest2 {
+
+    private int count = 0;
+    private String lockKey = "lock";
+
+    private void call(Jedis jedis) {
+        // 加锁
+        String requestId = UUID.randomUUID().toString();
+        boolean locked = RedisLock02.tryLock(jedis, lockKey,
+                requestId, 60);
+        try {
+            if (locked) {
+                for (int i =0; i < 500; i++) {
+                    count ++;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            RedisLock02.unlock(jedis, lockKey, requestId);
+        }
+    }
+
+}

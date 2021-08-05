8a9c5e549dd7b31c997a33ba670ea0978016df9d
“xiezeyi” <xiezy@miligc.com>
2021-08-05 15:26:32 +0800

    feat: [redis-lock-demo]互斥与互斥bug解决，接上一次commit

--- redis/redis-lock-demo/src/main/java/com/imooc/demo02/RedisLock02.java
+++ redis/redis-lock-demo/src/main/java/com/imooc/demo02/RedisLock02.java
@@ -10,6 +10,7 @@ public class RedisLock02 {
 
     /**
      * 尝试获取分布式锁
+     * 通过加锁和删锁都有一个唯一的requestId进行标识。判断要不要删锁
      * @param jedis Redis客户端
      * @param lockKey 锁
      * @param requestId 锁的值
@@ -32,6 +33,13 @@ public class RedisLock02 {
 
     /**
      * 释放分布式锁
+     * 释放锁时，先获取到锁的值，与该线程下的值进行比对判断，相同就解锁，不同则可能因为超时等原因导致锁已经被释放掉了
+     * 此时只需要直接返回即可
+     * 该情况是进程级别的，这里是一个JVM ，要在多个JVM下构建多个程序才可演示该效果
+     * 为什么教学说线程级别不能演示？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？
+     * 相关学习
+     * https://blog.csdn.net/weixin_39833509/article/details/88603957?utm_medium=distribute.pc_aggpage_search_result.none-task-blog-2~aggregatepage~first_rank_v2~rank_aggregation-1-88603957.pc_agg_rank_aggregation&utm_term=%E4%B8%80%E4%B8%AA%E8%AF%B7%E6%B1%82%E6%98%AF%E4%B8%80%E4%B8%AA%E7%BA%BF%E7%A8%8B%E8%BF%98%E6%98%AF%E4%B8%80%E4%B8%AA%E8%BF%9B%E7%A8%8B&spm=1000.2123.3001.4430
+     *
      * @param jedis Redis客户端
      * @param lockKey 锁
      * @return 是否释放成功
